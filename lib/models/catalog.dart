8e961085893027f2a28b83bc3f51585031dee6b5
Pranshu Pandey <pranshupandey069@gmail.com>
2023-10-28 13:40:46 +0530

    new model

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,66 +1,97 @@
+// To parse this JSON data, do
+//
+//     final catalog = catalogFromJson(jsonString);
+
+import 'dart:convert';
+
+Catalog catalogFromJson(String str) => Catalog.fromJson(json.decode(str));
+
+String catalogToJson(Catalog data) => json.encode(data.toJson());
+
+class Catalog {
+  List<Items> items;
+
+  Catalog({
+    required this.items,
+  });
+
+  factory Catalog.fromJson(Map<String, dynamic> json) => Catalog(
+        items: List<Items>.from(json["items"].map((x) => Items.fromJson(x))),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "items": List<dynamic>.from(items.map((x) => x.toJson())),
+      };
+}
+
 class Items {
-  final num id;
-  final String title;
-  final num price;
-  final String description;
-  final String catagory;
-  final num rating;
-  final String image;
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+  Rating rating;
 
   Items({
     required this.id,
     required this.title,
     required this.price,
     required this.description,
-    required this.catagory,
-    required this.rating,
+    required this.category,
     required this.image,
+    required this.rating,
   });
 
-  factory Items.fromMap(Map<String, dynamic> map) {
-    return Items(
-      id: map["id"],
-      title: map["title"],
-      price: map["price"],
-      description: map["description"],
-      catagory: map["catagory"],
-      rating: map["rating"],
-      image: map["image"],
-    );
-  }
+  factory Items.fromJson(Map<String, dynamic> json) => Items(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"]?.toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+        rating: Rating.fromJson(json["rating"]),
+      );
 
-  toMap() => {
+  Map<String, dynamic> toJson() => {
         "id": id,
         "title": title,
         "price": price,
         "description": description,
-        "rating": rating,
+        "category": category,
         "image": image,
-        "catagory": catagory,
+        "rating": rating.toJson(),
       };
 }
 
-class Catelog {
-  static List<Items> products = [];
-  // [
-  //   Items(
-  //       catagory: "men's clothing",
-  //       description:
-  //           "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-  //       id: 1.0,
-  //       price: 109.95,
-  //       rating: 3.9,
-  //       title: "Fjallraven - Foldsack No. 1 Backpack",
-  //       image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg'),
-  //   Items(
-  //     catagory: "men's clothing",
-  //     description:
-  //         "A versatile backpack for all your adventures. This pack includes a padded laptop sleeve for laptops up to 15 inches, making it ideal for work or outdoor trips.",
-  //     price: 129.99,
-  //     rating: 4.2,
-  //     image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-  //     id: 2,
-  //     title: 'Good iyems',
-  //   ),
-  // ];
+class Rating {
+  double rate;
+  int count;
+
+  Rating({
+    required this.rate,
+    required this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"]?.toDouble(),
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+      };
+}
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
 }
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -14,6 +14,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  dynamic productsdata;
+  Catalog? catalog;
   @override
   void initState() {
     super.initState();
@@ -23,77 +25,36 @@ class _HomePageState extends State<HomePage> {
   loadData() async {
     var json = await rootBundle.loadString("assets/files/catelog.json");
     var decodedjson = jsonDecode(json);
-    final productsdata = decodedjson["products"];
-    print(productsdata);
-    // print(Catelog.products);
-    // Catelog.products = productsdata.map((item) => Items.fromMap(item)).toList();
-    Catelog.products = [
-      Items(
-        id: 2,
-        catagory: "men's clothing",
-        description:
-            "A versatile backpack for all your adventures. This pack includes a padded laptop sleeve for laptops up to 15 inches, making it ideal for work or outdoor trips.",
-        price: 129.99,
-        rating: 4.2,
-        // image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-        image:
-            "https://fakestoreapi.com/img/61sbMiUnoGL._AC_UL640_QL65_ML3_.jpg",
-        title: 'Good items',
-      ),
-    ];
-    //     List.from(productsdata).map<Items>((e) => Items.fromMap(e)).toList();
+    await Future.delayed(Duration(seconds: 2));
+    productsdata = decodedjson["products"];
+
     setState(() {});
   }
-
-  // loadData() async {
-  //   try {
-  //     var json = await rootBundle.loadString("assets/files/catelog.json");
-  //     var decodedjson = jsonDecode(json);
-  //     final productsdata = decodedjson["products"];
-  //     Catelog.products =
-  //         productsdata.map((item) => Items.fromMap(item)).toList();
-
-  //     // Add your hardcoded items to the list
-  //     Catelog.products.addAll([
-  //       Items(
-  //         catagory: "men's clothing",
-  //         description:
-  //             "A versatile backpack for all your adventures. This pack includes a padded laptop sleeve for laptops up to 15 inches, making it ideal for work or outdoor trips.",
-  //         price: 129.99,
-  //         rating: 4.2,
-  //         image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
-  //         id: 2,
-  //         title: 'Good items',
-  //       ),
-  //     ]);
-
-  //     setState(() {});
-  //   } catch (error) {
-  //     // Handle any errors that may occur while loading or parsing the JSON data.
-  //     print("Error loading JSON data: $error");
-  //   }
-  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text("Catalog App"),
         centerTitle: true,
         // backgroundColor: Colors.lightBlue,
       ),
       drawer: MyDrawer(),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: Catelog.products.length,
-          itemBuilder: (context, index) {
-            return ItemWidgets(
-              product: Catelog.products[index],
-            );
-          },
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: productsdata == null
+              ? const Text("Loading")
+              : productsdata.length == 0
+                  ? const Text("No catalog found")
+                  : ListView.builder(
+                      itemCount: productsdata.length,
+                      itemBuilder: (context, index) {
+                        Items item = Items.fromJson(productsdata[index]);
+                        return ItemWidgets(
+                          product: item,
+                        );
+                      },
+                    )),
     );
   }
 }
